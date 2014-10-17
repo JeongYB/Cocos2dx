572760f5c44274ab6b4bf4a35977e492658ad40f
JeongYB <jmam0513@naver.com>
2014-10-17 16:41:48 +0900

    10-17
    DrawNode + undo

--- CocosJSGame/src/app.js
+++ CocosJSGame/src/app.js
@@ -2,71 +2,65 @@
 var HelloWorldLayer = cc.Layer.extend({
 	  sprite:null,
 	  ctor:function () {
-        //////////////////////////////
-        // 1. super init first
+      
         this._super();
-
-        /////////////////////////////
-        // 2. add a menu item with "X" image, which is clicked to quit the program
-        //    you may modify it.
-        // ask the window size
         var size = cc.winSize;
 
-        // add a "close" icon to exit the progress. it's an autorelease object
-        var closeItem = new cc.MenuItemImage(
-            res.CloseNormal_png,
-            res.CloseSelected_png,
-            function () {
-                cc.log("Menu is clicked!");
-            }, this);
-        closeItem.attr({
-            x: size.width - 20,
-            y: 20,
-            anchorX: 0.5,
-            anchorY: 0.5
-        });
+       
 
-        var menu = new cc.Menu(closeItem);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild(menu, 1);
-
-        /////////////////////////////
-        // 3. add your codes below...
-        // add a label shows "Hello World"
-        // create and initialize a label
         var helloLabel = new cc.LabelTTF("TheWol", "Arial", 38);
-        // position the label on the center of the screen
-        
         helloLabel.x = size.width / 2;
         helloLabel.y = size.height - 20;
-        // add the label as a child to this layer
-        this.addChild(helloLabel, 5);
+         this.addChild(helloLabel, 5);
+         
+         this.sprite = new cc.Sprite("res/Wolf.png");
+         this.sprite.attr({
 
-        // add "HelloWorld" splash screen"
-        this.sprite = new cc.Sprite("res/Wolf.png");
-        this.sprite.attr({
-        	
-            x: size.width / 2 ,
-            y: size.height/ 2  ,
-            
+        	 x: size.width / 2 ,
+        	 y: size.height/ 2  ,
+
+         });
+         this.addChild(this.sprite, 0);
+
+        
+        // add the DrawNode 
+        this.drawNode = new cc.DrawNode(); 
+        this.addChild(this.drawNode); 
+
+        //add the Listener
+        var listener = cc.EventListener.create({
+        	event:cc.EventListener.TOUCH_ONE_BY_ONE,
+        	swallowTouches:true,
+        	onTouchBegan:function(touch,event){
+        		return true;
+        	},
+        	onTouchMoved:function(touch,event){
+        		return true;
+        	},
+        	onTouchEnded:function(touch,event){
+        		var p1 = touch.getStartLocation();
+        		var p2 = touch.getLocation();
+        		event.getCurrentTarget().shapes.push(p1,p2);
+        		event.getCurrentTarget().drawNode.drawSegment(p1,p2,2,cc.color(100,20,20));
+        		return true;
+        	}
         });
-        this.addChild(this.sprite, 0);
-
-        this.sprite.runAction(
-            cc.sequence(
-                cc.rotateTo(2, 0),
-                cc.scaleTo(2, 1, 1)
-            )
-        );
-        //helloLabel.runAction(
-          //  cc.spawn(
-            //    cc.moveBy(2.5, cc.p(0, size.height - 40)),
-             //   cc.tintTo(2.5,255,125,0)
-           // )
-      //  );
+        cc.eventManager.addListener(listener, this);
+        
+        
+        
+        
         return true;
-    }
+    },
+    undo: function(){
+    	this.shapes.pop();
+    	this.shapes.pop();
+    	this.drawNode.clear();
+    	for(var i = 0; i<this.shapes.length;i+=2){
+    		this.drawNode.drawSegment(this.shapes[i],this.shapes[i+1],2,cc.color(100,20,20));
+    	}
+     }
+    
 });
 
 var HelloWorldScene = cc.Scene.extend({
